900e7fd07eaebe606ee62a00956ee3abc102cc9e
Leons Buntis <leons.buntis@if.lv>
2022-11-05 09:27:55 +0200

    asd

--- index.js
+++ index.js
@@ -19,8 +19,39 @@ bot.help((ctx) => ctx.reply('Help!'));
 // bot.command('subscribe', (ctx) => Commands.subscribe(ctx.update.message.chat.id, ctx.reply));
 
 // Handlers
-bot.hears((message, { reply }) => {
-    reply(message + ' 42');
-});
+
+bot.inlineQuery((query, ctx) => {
+    console.log(query);
+    console.log(ctx);
+    
+    ctx.answerInlineQuery([{
+        type: 'article',
+        id: 1,
+        title: 'test title',
+        input_message_content: 'test contents',
+        photo_url: 'https://upload.wikimedia.org/wikipedia/commons/b/ba/John_Broadus_Watson.JPG'
+      }])
+})
+
+// bot.hears((message, { reply, replyWithChatAction, replyWithQuiz }) => {
+//     console.log(message);
+//     if (message.toLowerCase().indexOf('stupid') != -1) {
+//         // replyWithPoll(
+//         //     'Is that a fact or opinion?',
+//         //     ['Fact', 'Opinion']
+//         // );
+
+//         replyWithChatAction('typing').then(_ => {
+
+//             replyWithQuiz(
+//                 'Is that a fact or opinion?',
+//                 ['Fact', 'Opinion'],
+//             );
+//         });
+
+//         return;
+//     }
+//     reply(message + ' 42');
+// });
 
 module.exports = bot;
