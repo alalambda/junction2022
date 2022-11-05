47eb5c331149bdf73049a8f0a2e28cb65248962b
Leons Buntis <leons.buntis@if.lv>
2022-11-05 22:20:25 +0200

    cxzcxzcxz

--- index.js
+++ index.js
@@ -28,38 +28,52 @@ bot.start((ctx) => {
 });
 
 bot.on("text", async (ctx) => {
-    const msg = ctx.message.text.toLowerCase();
+    if (ctx.message.text.indexOf('hi') > -1) {
+        ctx.reply(
+            'Special buttons keyboard',
+            Markup.keyboard([
+                Markup.button.callback('Send contact', 'asd'),
+                Markup.button.callback('Send location', 'asds')
+            ])
+        )        
+    } else {
+        ctx.reply('asdsadas')
+    }
+})
 
-    if (msg.indexOf('I am a failure. I always ruin everything.') != -1) {
-        ctx.reply('What event caused these thoughts?');
-    } else if (msg.indexOf('I felt lonely, anxious and afraid, I couldn’t speak.') != -1) {
-        ctx.reply('You said that you felt lonely, anxious and afraid. These thoughts lead you thinking that you are a failure and you always ruin everything. Is that correct?');
-    } else if (msg.indexOf('Yes.') != -1) {
-        ctx.reply('Given the situation, how would you rate your belief in your statement that you are ‘a failure and ruin everything’?')
-    } else if (msg.indexOf('90%') != -1) {
-        ctx.reply(' - Given the extreme nature of this judgement, a profound feeling of loneliness, anxiety and fear is understandable.'
-            + ' - The thought ‘I am a failure. I always ruin everything.’ Is the hidden meaning your mind has assigned to this event.'
-            + ' - What would be the worst-case scenario, and why?')
-    } else if (msg.indexOf('People would think I don’t like them and wouldn’t want to hang out with me anymore. I will lose all my friends because they are ashamed of my behaviour.') != -1) {
-        ctx.reply('Okay. And what would be the best-case scenario in your described situation?');
-    } else if (msg.indexOf('People would understand that I got nervous and would offer me support.') != -1) {
-        ctx.reply('Did your described best case scenario ever happen in your life?');
-    } else if (msg.indexOf('Yes') != -1) {
-        ctx.reply('Did your described worst case scenario ever happen in your life?')
-    } else if (msg.indexOf('No') != -1) {
-        ctx.reply('Given the worst and the best case scenarios that you described, how would you rate your belief in your initial thoughts that you are ‘a failure and ruin everything’?');
-    } else if (msg.indexOf('25%') != -1) {
-        ctx.reply('Your belief in destructive thoughts significantly decreased, which is really good.'
-            + ' - Do you want to share your answers with your mental health professional?');
-    } else if (msg.indexOf('Yes') != -1) {
-        ctx.reply(' Thank you for being open with me today. Your answers will be shared with your mental health professional.'
-            + ' - Try to do something for yourself today, like: taking a bath, ordering favourite food or reading a good book. '
-            + ' - Have a nice day/evening.')
-    }
-    else {
-        ctx.reply('I\'m sorry, but I\'m not that smart yet to help you out.');
-    }
-});
+// bot.on("text", async (ctx) => {
+//     const msg = ctx.message.text.toLowerCase();
+
+//     if (msg.indexOf('I am a failure. I always ruin everything.') != -1) {
+//         ctx.reply('What event caused these thoughts?');
+//     } else if (msg.indexOf('I felt lonely, anxious and afraid, I couldn’t speak.') != -1) {
+//         ctx.reply('You said that you felt lonely, anxious and afraid. These thoughts lead you thinking that you are a failure and you always ruin everything. Is that correct?');
+//     } else if (msg.indexOf('Yes.') != -1) {
+//         ctx.reply('Given the situation, how would you rate your belief in your statement that you are ‘a failure and ruin everything’?')
+//     } else if (msg.indexOf('90%') != -1) {
+//         ctx.reply(' - Given the extreme nature of this judgement, a profound feeling of loneliness, anxiety and fear is understandable.'
+//             + ' - The thought ‘I am a failure. I always ruin everything.’ Is the hidden meaning your mind has assigned to this event.'
+//             + ' - What would be the worst-case scenario, and why?')
+//     } else if (msg.indexOf('People would think I don’t like them and wouldn’t want to hang out with me anymore. I will lose all my friends because they are ashamed of my behaviour.') != -1) {
+//         ctx.reply('Okay. And what would be the best-case scenario in your described situation?');
+//     } else if (msg.indexOf('People would understand that I got nervous and would offer me support.') != -1) {
+//         ctx.reply('Did your described best case scenario ever happen in your life?');
+//     } else if (msg.indexOf('Yes') != -1) {
+//         ctx.reply('Did your described worst case scenario ever happen in your life?')
+//     } else if (msg.indexOf('No') != -1) {
+//         ctx.reply('Given the worst and the best case scenarios that you described, how would you rate your belief in your initial thoughts that you are ‘a failure and ruin everything’?');
+//     } else if (msg.indexOf('25%') != -1) {
+//         ctx.reply('Your belief in destructive thoughts significantly decreased, which is really good.'
+//             + ' - Do you want to share your answers with your mental health professional?');
+//     } else if (msg.indexOf('Yes') != -1) {
+//         ctx.reply(' Thank you for being open with me today. Your answers will be shared with your mental health professional.'
+//             + ' - Try to do something for yourself today, like: taking a bath, ordering favourite food or reading a good book. '
+//             + ' - Have a nice day/evening.')
+//     }
+//     else {
+//         ctx.reply('I\'m sorry, but I\'m not that smart yet to help you out.');
+//     }
+// });
 
 // bot.on("text", async (ctx) => {
 //     let results = await client.analyzeSentiment([ctx.message.text]);
--- test-bot.ts
+++ test-bot.ts
@@ -19,37 +19,41 @@ greeterScene.on("text", ctx => {
 const analyzeFirstTought = "analyzeFirstTought";
 const analyzeFirstToughtScene = new Scenes.BaseScene<Scenes.SceneContext>(analyzeFirstTought);
 analyzeFirstToughtScene.enter(ctx => ctx.reply("What event caused these thoughts?"));
-analyzeFirstToughtScene.on("text", ctx => {
-	ctx.replyWithHTML("You said that you felt <b>lonely</b>, <b>anxious</b> and <b>afraid</b>. These thoughts lead you thinking that you are a <b>failure</b> and you <b>always ruin everything</b>. Is that correct?",
-		Markup.inlineKeyboard([
-			Markup.button.callback('Yes', 'yes'),
-			Markup.button.callback('No', 'no')
-		]));
-});
-analyzeFirstToughtScene.action('yes', async ctx => {
-	await ctx.editMessageReplyMarkup(undefined);
+analyzeFirstToughtScene.hears('Correct', async ctx => {
 	await ctx.replyWithHTML("Given the situation, how would you rate your belief in your statement that you are ‘a failure and ruin everything’?",
-		Markup.inlineKeyboard([
+		Markup.keyboard([
 			Markup.button.callback('Strong', 'strong'),
 			Markup.button.callback('Medium', 'medium'),
 			Markup.button.callback('Little', 'little')
-		]))
+		])
+		.oneTime()
+		.resize())
 });
-analyzeFirstToughtScene.action('no', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `Yes`'));
-
-analyzeFirstToughtScene.action('strong', async ctx => {
-	await ctx.editMessageReplyMarkup(undefined);
+analyzeFirstToughtScene.hears('No', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `Yes`'));
+analyzeFirstToughtScene.hears('Strong', async ctx => {
 	await ctx.scene.enter(worstCase);
 });
-analyzeFirstToughtScene.action('medium', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `Strong`'));
-analyzeFirstToughtScene.action('little', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `Strong`'));
+analyzeFirstToughtScene.hears('Medium', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `Strong`'));
+analyzeFirstToughtScene.hears('Little', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `Strong`'));
+analyzeFirstToughtScene.on("text", ctx => {
+	ctx.replyWithHTML("You said that you felt <b>lonely</b>, <b>anxious</b> and <b>afraid</b>. These thoughts lead you thinking that you are a <b>failure</b> and you <b>always ruin everything</b>. Is that correct?",
+		Markup
+			.keyboard([
+				Markup.button.callback('Correct', 'yes'),
+				Markup.button.callback('No', 'no')
+			])
+			.oneTime()
+			.resize()
+	);
+});
+
 
 // belief 1 
 const worstCase = "worstCase";
 const worstCaseScene = new Scenes.BaseScene<Scenes.SceneContext>(worstCase);
 worstCaseScene.enter(ctx => ctx.replyWithHTML(
 	'Given the extreme nature of this judgement, a profound feeling of loneliness, anxiety and fear is understandable.'
-	+ '\nThe thought ‘I am a failure. I always ruin everything.’ Is the hidden meaning your mind has assigned to this event.'
+	+ '\nThe thought ‘<i>I am a failure. I always ruin everything.</i>’ Is the hidden meaning your mind has assigned to this event.'
 	+ '\nWhat would be the worst-case scenario, and why?'));
 worstCaseScene.on("text", async ctx => {
 	// txt should be 
@@ -69,52 +73,56 @@ bestCaseScenario.on("text", async ctx => {
 const worstCase2 = "worstCase2";
 const worstCase2Scene = new Scenes.BaseScene<Scenes.SceneContext>(worstCase2);
 worstCase2Scene.enter(ctx => ctx.replyWithHTML("Did your described <b>best</b> case scenario ever happen in your life?",
-	Markup.inlineKeyboard([
+	Markup.keyboard([
 		Markup.button.callback('Yes', 'yes'),
 		Markup.button.callback('No', 'no')
-	])));
-worstCase2Scene.action('yes', async ctx => {
-	await ctx.editMessageReplyMarkup(undefined);
+	])
+	.oneTime()
+	.resize()
+	));
+worstCase2Scene.hears('Yes', async ctx => {
 	await ctx.replyWithHTML("Did your described <b>worst</b> case scenario ever happen in your life?",
-		Markup.inlineKeyboard([
-			Markup.button.callback('Yes', 'yes2'),
-			Markup.button.callback('No', 'no2')
-		]))
+		Markup.keyboard([
+			Markup.button.callback('Yeah', 'yes2'),
+			Markup.button.callback('No, not really', 'no2')
+		])
+		.oneTime()
+		.resize())
 });
-worstCase2Scene.action('no', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `Yes`'));
+worstCase2Scene.hears('No', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `Yes`'));
 
-worstCase2Scene.action('no2', async ctx => {
-	await ctx.editMessageReplyMarkup(undefined);
+worstCase2Scene.hears('No, not really', async ctx => {
 	await ctx.replyWithHTML('Given the worst and the best case scenarios that you described, how would you rate your belief in your initial thoughts that you are ‘a failure and ruin everything’?',
-		Markup.inlineKeyboard([
+		Markup.keyboard([
 			Markup.button.callback('Strong', 'strong'),
 			Markup.button.callback('Medium', 'medium'),
 			Markup.button.callback('Little', 'little')
-		]))
+		])
+		.oneTime()
+		.resize())
 })
-worstCase2Scene.action('yes2', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `No`'));
+worstCase2Scene.hears('Yeah', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `No`'));
 
-worstCase2Scene.action('little', async ctx => {
-	await ctx.editMessageReplyMarkup(undefined);
+worstCase2Scene.hears('Little', async ctx => {
 	await ctx.replyWithHTML('Your belief in destructive thoughts significantly decreased, which is really good.'
 		+ '\nDo you want to share your answers with your mental health professional?',
-		Markup.inlineKeyboard([
-			Markup.button.callback('Yes', 'share'),
-			Markup.button.callback('No', 'dontShare'),
-		]))
+		Markup.keyboard([
+			Markup.button.callback('Share', 'share'),
+			Markup.button.callback('Don\'t share', 'dontShare'),
+		])
+		.oneTime()
+		.resize())
 });
-worstCase2Scene.action('medium', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `little`'));
-worstCase2Scene.action('strong', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `little`'));
+worstCase2Scene.hears('Medium', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `little`'));
+worstCase2Scene.hears('Strong', ctx => ctx.reply('sorry I\'m not programmed to this route, try answering `little`'));
 
-worstCase2Scene.action('share', async ctx => {
-	await ctx.editMessageReplyMarkup(undefined);
+worstCase2Scene.hears('Share', async ctx => {
 	await ctx.replyWithHTML('Thank you for being open with me today. Your answers will be shared with your mental health professional.'
 		+ '\nTry to do something for yourself today, like: taking a bath, ordering favourite food or reading a good book. '
 		+ '\nHave a nice day/evening.');
 	await ctx.scene.leave();
 });
-worstCase2Scene.action('dontShare', async ctx => {
-	await ctx.editMessageReplyMarkup(undefined);
+worstCase2Scene.hears('Don\'t share', async ctx => {
 	await ctx.replyWithHTML('Thank you for being open with me today. Your answers won\'t be shared with anyone!'
 		+ '\nTry to do something for yourself today, like: taking a bath, ordering favourite food or reading a good book. '
 		+ '\nHave a nice day/evening.');
@@ -137,8 +145,7 @@ const stage = new Scenes.Stage<Scenes.SceneContext>([
 
 bot.use(session());
 bot.use(stage.middleware());
-bot.command(greeter, ctx => ctx.scene.enter(greeter));
-bot.on("message", ctx => ctx.reply("Try /greeter"));
+bot.on("message", ctx => ctx.scene.enter(greeter));
 
 bot.launch();
 
